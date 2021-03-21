6c329da3338e63ace769dae63d94c7d0403d68c8
kimyang-sun <berman532@gmail.com>
2021-03-22 01:54:15 +0900

    쿠키저장 1차

--- app.js
+++ app.js
@@ -49,6 +49,10 @@ app.use(
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      domain: '.write-mind.vercel.app',
+    },
   })
 );
 app.use(passport.initialize());
